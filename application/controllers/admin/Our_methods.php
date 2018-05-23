b87007b63c412d24bd902a117ffc3752aba22a73
minhtruongmato <truong.do@matocreative.vn>
2018-05-23 16:32:37 +0700

    [done] implement our methods function in admin

--- application/controllers/admin/Our_methods.php
+++ application/controllers/admin/Our_methods.php
@@ -1,22 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Our_message extends Admin_Controller {
+class Our_methods extends Admin_Controller {
 	private $request_language_template = array(
         'description'
     );
     private $author_data = array();
     function __construct(){
         parent::__construct();
-        $this->load->model('our_message_model');
+        $this->load->model('our_methods_model');
         $this->load->helper('common');
         $this->author_data = handle_author_common_data();
     }
 	public function index(){
-        $total_rows  = $this->our_message_model->count_by_our_message();
+		$total_rows  = $this->our_methods_model->count_by_our_methods();
         $this->load->library('pagination');
         $config = array();
-        $base_url = base_url('admin/our_message/index');
+        $base_url = base_url('admin/our_methods/index');
         $per_page = 10;
         $uri_segment = 4;
         foreach ($this->pagination_config($base_url, $total_rows, $per_page, $uri_segment) as $key => $value) {
@@ -27,9 +27,9 @@ class Our_message extends Admin_Controller {
         $this->data['page_links'] = $this->pagination->create_links();
         $this->data['page'] = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
 
-        $result = $this->our_message_model->get_all_by_our_message_with_pagination_search($per_page, $this->data['page']);
+        $result = $this->our_methods_model->get_all_by_our_methods_with_pagination_search($per_page, $this->data['page']);
         $this->data['result'] = $result;
-        $this->render('admin/our_message/list_our_message_view');
+        $this->render('admin/our_methods/list_our_methods_view');
 	}
 
 	public function create() {
@@ -40,27 +40,27 @@ class Our_message extends Admin_Controller {
         $this->form_validation->set_rules('description_en', 'Description', 'trim|required');
 
         if ($this->form_validation->run() == FALSE) {
-        	$this->render('admin/our_message/create_our_message_view');
+        	$this->render('admin/our_methods/create_our_methods_view');
         } else {
         	if ($this->input->post()) {
         		$shared_request = array();
         		$this->db->trans_begin();
 
-        		$insert = $this->our_message_model->common_insert(array_merge($shared_request, $this->author_data));
+        		$insert = $this->our_methods_model->common_insert(array_merge($shared_request, $this->author_data));
         		if($insert){
-        			$requests = handle_multi_language_request('our_message_id', $insert, $this->request_language_template, $this->input->post(), $this->page_languages);
-        			$this->our_message_model->insert_with_language($requests);
+        			$requests = handle_multi_language_request('our_methods_id', $insert, $this->request_language_template, $this->input->post(), $this->page_languages);
+        			$this->our_methods_model->insert_with_language($requests);
         		}
 
         		if ($this->db->trans_status() === false) {
         			$this->db->trans_rollback();
         			$this->load->libraries('session');
         			$this->session->set_flashdata('message_error', 'Thêm mới thất bại!');
-        			$this->render('admin/our_message/create_our_message_view');
+        			$this->render('admin/our_methods/create_our_methods_view');
         		} else {
         			$this->db->trans_commit();
         			$this->session->set_flashdata('message_success', 'Thêm mới thành công!');
-        			redirect('admin/our_message', 'refresh');
+        			redirect('admin/our_methods', 'refresh');
         		}
         	}
         }
@@ -70,18 +70,18 @@ class Our_message extends Admin_Controller {
     	$this->load->helper('form');
         $this->load->library('form_validation');
 
-    	$detail = $this->our_message_model->get_by_id($id, $this->request_language_template);
+    	$detail = $this->our_methods_model->get_by_id($id, $this->request_language_template);
         if(!$detail['id']){
-            redirect('admin/our_message/index','refresh');
+            redirect('admin/our_methods/index','refresh');
         }
-        $detail = build_language('our_message', $detail, $this->request_language_template, $this->page_languages);
+        $detail = build_language('our_methods', $detail, $this->request_language_template, $this->page_languages);
         $this->data['detail'] = $detail;
 
         $this->form_validation->set_rules('description_vi', 'Giới thiệu', 'trim|required');
         $this->form_validation->set_rules('description_en', 'Description', 'trim|required');
 
         if ($this->form_validation->run() == FALSE) {
-        	$this->render('admin/our_message/edit_our_message_view');
+        	$this->render('admin/our_methods/edit_our_methods_view');
         } else {
         	if ($this->input->post()) {
         		$shared_request = array(
@@ -89,11 +89,11 @@ class Our_message extends Admin_Controller {
                     'updated_by' => $this->author_data['updated_by']
         		);
         		$this->db->trans_begin();
-        		$update = $this->our_message_model->common_update($id, $shared_request);
+        		$update = $this->our_methods_model->common_update($id, $shared_request);
                     if($update){
-                        $requests = handle_multi_language_request('our_message_id', $id, $this->request_language_template, $this->input->post(), $this->page_languages);
+                        $requests = handle_multi_language_request('our_methods_id', $id, $this->request_language_template, $this->input->post(), $this->page_languages);
                         foreach ($requests as $key => $value){
-                            $this->our_message_model->update_with_language($id, $requests[$key]['language'], $value);
+                            $this->our_methods_model->update_with_language($id, $requests[$key]['language'], $value);
                         }
                     }
 
@@ -101,11 +101,11 @@ class Our_message extends Admin_Controller {
                         $this->db->trans_rollback();
                         $this->load->libraries('session');
                         $this->session->set_flashdata('message_error', 'Cập nhật thất bại!');
-                        $this->render('admin/our_message/edit/'.$id);
+                        $this->render('admin/our_methods/edit/'.$id);
                     } else {
                         $this->db->trans_commit();
                         $this->session->set_flashdata('message_success', 'Cập nhật thành công!');
-                        redirect('admin/our_message', 'refresh');
+                        redirect('admin/our_methods', 'refresh');
                     }
         	}
         }
@@ -114,7 +114,7 @@ class Our_message extends Admin_Controller {
     public function remove(){
         $id = $this->input->post('id');
         $data = array('is_deleted' => 1);
-        $update = $this->our_message_model->common_update($id, $data);
+        $update = $this->our_methods_model->common_update($id, $data);
         if($update == 1){
             $reponse = array(
                 'csrf_hash' => $this->security->get_csrf_hash()
@@ -132,5 +132,5 @@ class Our_message extends Admin_Controller {
 
 }
 
-/* End of file Our_message.php */
-/* Location: ./application/controllers/admin/Our_message.php */
+/* End of file Our_methods.php */
+/* Location: ./application/controllers/admin/Our_methods.php */

8aaee0c123cac5070e1b2c675834b7d5191c8eb1
hungluong249 <hung.luong@matocreative.vn>
2018-06-01 09:26:10 +0700

    fix meta tag for unchanged page

--- application/views/templates/public_parts/master_header_view.php
+++ application/views/templates/public_parts/master_header_view.php
@@ -7,8 +7,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="<?php echo (isset($meta_description))? $meta_description : '' ?>" />
-	<meta name="keywords" content="<?php echo (isset($meta_keywords))? $meta_keywords : '' ?>" />
+    <meta name="description" content="<?php echo (isset($meta_description))? $meta_description : 'Trung tâm Tiếng Anh myIELTS - Make IELTS Meaningful' ?>" />
+	<meta name="keywords" content="<?php echo (isset($meta_keywords))? $meta_keywords : 'myIELTS, IELTS, learning' ?>" />
     <title>myIELTS</title>
 
 	<!-- FAVICON -->

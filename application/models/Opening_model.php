6d58ccc8963261ca09a3eb249040d2ec35114c5d
minhtruongmato <truong.do@matocreative.vn>
2018-06-02 11:25:14 +0700

    [done] implement opening function in admin

---
+++ application/models/Opening_model.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Opening_model extends MY_Model {
+
+	public $table = 'opening';
+	public function __construct() {
+        parent::__construct();
+    }
+
+}
+
+/* End of file Opening_model.php */
+/* Location: ./application/models/Opening_model.php */
